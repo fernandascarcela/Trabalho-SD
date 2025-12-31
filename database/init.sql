678dd9524d4b6190fcdd852889f35ad9223cb727
Giovanna Verissimo <giovannaxavier04@gmail.com>
2025-12-31 01:39:57 -0300

    Add BD e serviços de notificações com RabbitMQ

---
+++ database/init.sql
@@ -0,0 +1,166 @@
+CREATE TYPE user_type_enum AS ENUM ('PACIENTE', 'MEDICO', 'RECEPCIONISTA', 'ADMIN');
+CREATE TYPE weekday_enum AS ENUM ( 'SEGUNDA', 'TERCA', 'QUARTA', 'QUINTA', 'SEXTA');
+CREATE TYPE appointment_status_enum AS ENUM ('AGENDADO', 'CONFIRMADO', 'CANCELADO', 'CONCLUÍDO', 'REJEITADA');
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    user_type user_type_enum NOT NULL
+);
+
+CREATE TABLE patient (
+    patient_id SERIAL PRIMARY KEY,
+    user_id INT UNIQUE NOT NULL,
+    cpf VARCHAR(14) UNIQUE NOT NULL,
+    birth_date DATE NOT NULL,
+
+    CONSTRAINT fk_patient_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(user_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE doctor (
+    doctor_id SERIAL PRIMARY KEY,
+    user_id INT UNIQUE NOT NULL,
+    crm VARCHAR(20) UNIQUE NOT NULL,
+    specialty VARCHAR(100) NOT NULL,
+
+    CONSTRAINT fk_doctor_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(user_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE insurance (
+    insurance_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE schedule (
+    schedule_id SERIAL PRIMARY KEY,
+    weekday weekday_enum NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+
+    CONSTRAINT valid_schedule CHECK (start_time < end_time)
+);
+
+CREATE TABLE appointment (
+    appointment_id SERIAL PRIMARY KEY,
+    patient_id INT NOT NULL,
+    doctor_id INT NOT NULL,
+    insurance_id INT,
+    schedule_id INT NOT NULL,
+    status appointment_status_enum DEFAULT 'AGENDADO',
+
+    CONSTRAINT fk_appointment_patient
+        FOREIGN KEY (patient_id)
+        REFERENCES patient(patient_id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_appointment_doctor
+        FOREIGN KEY (doctor_id)
+        REFERENCES doctor(doctor_id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_appointment_insurance
+        FOREIGN KEY (insurance_id)
+        REFERENCES insurance(insurance_id),
+
+    CONSTRAINT fk_appointment_schedule
+        FOREIGN KEY (schedule_id)
+        REFERENCES schedule(schedule_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE notification (
+    notification_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    appointment_id INT NOT NULL,
+    message TEXT NOT NULL,
+    read BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_notification_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(user_id),
+
+    CONSTRAINT fk_notification_appointment
+        FOREIGN KEY (appointment_id)
+        REFERENCES appointment(appointment_id)
+);
+
+CREATE OR REPLACE FUNCTION generate_appointment_notification()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF OLD.status IS DISTINCT FROM NEW.status THEN
+        INSERT INTO notification (user_id, appointment_id, message)
+        VALUES (
+            (SELECT user_id
+             FROM patient
+             WHERE patient_id = NEW.patient_id),
+            NEW.appointment_id,
+            'Status da consulta alterado para ' || NEW.status
+        );
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER trigger_appointment_status
+AFTER UPDATE OF status ON appointment
+FOR EACH ROW
+EXECUTE FUNCTION generate_appointment_notification();
+
+
+CREATE OR REPLACE FUNCTION notify_new_notification()
+RETURNS TRIGGER AS $$
+BEGIN
+    PERFORM pg_notify(
+        'new_notification',
+        NEW.notification_id::text
+    );
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER trigger_new_notification
+AFTER INSERT ON notification
+FOR EACH ROW
+EXECUTE FUNCTION notify_new_notification();
+
+-- MOCKANDO HORARIOS
+INSERT INTO schedule (weekday, start_time, end_time)
+VALUES
+('SEGUNDA', '08:00', '10:00'),
+('SEGUNDA', '10:00', '12:00'),
+('SEGUNDA', '13:00', '15:00'),
+('SEGUNDA', '15:00', '17:00'),
+
+('TERCA', '08:00', '10:00'),
+('TERCA', '10:00', '12:00'),
+('TERCA', '13:00', '15:00'),
+('TERCA', '15:00', '17:00'),
+
+('QUARTA', '08:00', '10:00'),
+('QUARTA', '10:00', '12:00'),
+('QUARTA', '13:00', '15:00'),
+('QUARTA', '15:00', '17:00'),
+
+('QUINTA', '08:00', '10:00'),
+('QUINTA', '10:00', '12:00'),
+('QUINTA', '13:00', '15:00'),
+('QUINTA', '15:00', '17:00'),
+
+('SEXTA', '08:00', '10:00'),
+('SEXTA', '10:00', '12:00'),
+('SEXTA', '13:00', '15:00'),
+('SEXTA', '15:00', '17:00');
+
+
