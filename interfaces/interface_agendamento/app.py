5d0040797d35be2c4bc4e4d11a74dd2893bb585f
Victor <victor.work.dev@gmail.com>
2026-01-03 23:39:09 -0300

    Conlusão de todas as funções de scripts e interfaces

--- interfaces/interface_agendamento/app.py
+++ interfaces/interface_agendamento/app.py
@@ -178,5 +178,45 @@ def consultas_agendadas(perfil_operador):
     except Exception as e:
         return jsonify({"erro": str(e)}), 500
 
+@app.post("/agendamentos/agendar")
+def agendar_consulta(perfil_operador):
+    if perfil_operador not in ["admin", "recepcionista", "paciente"]:
+        return erro("Perfil sem permissão para agendar consultas")
+    
+    if not request.is_json:
+        return erro("JSON inválido")
+
+    body = request.json
+    obrigatorios = ["perfil_operador", "email_operador", "senha_operador", "id_consulta", "forma_pagamento"]
+
+    if not all(c in body for c in obrigatorios):
+        return jsonify({"erro": "Campos obrigatórios ausentes"}), 400
+    
+    if body["forma_pagamento"] == "cartao":
+        if "numero_cartao" not in body or not body["numero_cartao"]:
+            return erro("Número do cartão é obrigatório")
+
+        if "data_validade" not in body or not body["data_validade"]:
+            return erro("Data de validade do cartão é obrigatória")
+
+    try:
+        resposta = rpc_client.agendar_consulta(
+            body["perfil_operador"],
+            body["email_operador"],
+            body["senha_operador"],
+            body["id_consulta"],
+            body["forma_pagamento"]
+
+        )
+        return jsonify(resposta), 200
+
+    except Exception as e:
+        return jsonify({"erro": str(e)}), 500
+    
+    #Serviço: se a forma de pagamento for cartão, verificar se a data de validade é válida
+    #Serviço: se a forma de pagamento for convenio, verificar através do email do paciente \
+                # se ele tiver sido validado através do script validacao_convenio, agenda, caso contrario, nao agenda
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)
